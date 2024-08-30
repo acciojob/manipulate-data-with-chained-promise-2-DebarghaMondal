0177bae0e3f2962fba1e367ec8e3c4ecaec757ed
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-30 10:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function getNumbers() {
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve([1, 2, 3, 4]);
+		}, 3000)
+	});
+}
+
+getNumbers().then(numbers =>{
+	return new Promise((resolve, reject)=>{
+		let evenNumber = numbers.filter(number => number % 2 === 0);
+		setTimeout(()=>{
+			resolve(evenNumbers);
+		}, 1000)
+	});
+})
+
+.then(evenNumbers => {
+	return new Promise((resolve, reject)=>{
+		let doubledNumbers = evenNumbers.map(number => number * 2);
+		setTimeout(()=>{
+			resolve(doubledNumbers);
+		}, 2000)
+	});
+})
+
+.then(doubledNumbers =>{
+	let outputElement = document.getElementById('output');
+	 outputElement.innerText = doubledNumbers.join(',');
+});
+
+
